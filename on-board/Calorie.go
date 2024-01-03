ac3b629a2e34f071740e2fe142ca2d40708f8c90
Alan Lin <alin@apexclearing.com>
2024-01-03 12:14:35 -0600

    convert the calorie program to a bazel project

--- on-board/Calorie.go
+++ on-board/Calorie.go
@@ -3,31 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"os"
 	"strconv"
 	"strings"
 )
 
-var reader *bufio.Reader
-
-func countSum() (int, string) {
+func countSum(scanner *bufio.Scanner) (int, string) {
 	sum := 0
 
-	for {
-		text, err := reader.ReadString('\n')
-		text = strings.TrimRight(text, "\n") // Consume the next line character
-		if text == "" {
-			break
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		if line == "" {
+			return sum, "continue"
 		}
-		if err == io.EOF {
-			return sum, "end"
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			return 0, "invalid number"
 		}
-		num, _ := strconv.Atoi(text)
+
 		sum += num
 	}
 
-	return sum, "continue"
+	return sum, "end"
 }
 
 func max(a, b int) int {
@@ -37,22 +35,37 @@ func max(a, b int) int {
 	return b
 }
 
-
 func main() {
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+	text := `1000
+	2000
+	3000
 
-	reader = bufio.NewReader(file)
+	4000
+
+	5000
+	6000
+
+	7000
+	8000
+	9000
+
+	10000`
+
 	maximum := 0
-	for {
-		sum, status := countSum()
-		if status == "end" {
-			break
-		}
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	sum := 0
+	
+	end := "continue"
+
+	for end == "continue" {
 		maximum = max(maximum, sum)
+		sum, end = countSum(scanner)
 	}
+
+	if end == "invalid number" {
+		fmt.Println("Invalid number")
+		return
+	}
+
 	fmt.Println(maximum)
 }
